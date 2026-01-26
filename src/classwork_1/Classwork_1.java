f88fa109de26dc4f9f19285e2808053fbdc31f32
aydinsamiyev01-creator <aydinsamiyev01@gmail.com>
2026-01-26 19:54:03 +0400

    homework_2

--- src/classwork_1/Classwork_1.java
+++ src/classwork_1/Classwork_1.java
@@ -17,16 +17,53 @@ public class Classwork_1 {
 	        System.out.println("Холодно, одевайтесь теплее");
 		 }
 		 //3
-		 int mark = 4
-		 if (mark > 4)
+		 int mark = 4;
+		 if (mark > 4) {
 		 System.out.println("Great !");
 	     } else if (mark < 5) {
 	     System.out.println("Normal!");
 	     }else {
-	     System.out.println("Bad !")
+	     System.out.println("Bad !");
 	    }
-	}
+		 
+		//5
+	     
+	    int day = 7;;
+	        switch(day) {
+	        case 1 -> System.out.println("Понедельник");
+	        case 2 -> System.out.println("Вторник");
+	        case 3 -> System.out.println("Среда");
+	        case 4 -> System.out.println("Четверг");
+	        case 5 -> System.out.println("Пятница");
+	        case 6 -> System.out.println("Суббота");
+	        case 7 -> System.out.println("Воскресенье");
+	        }	  
+	        
+	        
+	        
+	        // 6
+	        int grade = 5;
+	        switch(grade) {
+	        case 'A' -> System.out.println("Отлично");
+	        case 'B' -> System.out.println("Хорошо");
+	        case 'C' -> System.out.println("Средне");
+	        case 'D' -> System.out.println("Плохо");
+	        case 'F' -> System.out.println("Не сдал");
+	        }
+	        //7
+	        
+	        	
+	    
+	      
+	        
+	        
+	        
+	        
+	        
+  }
+
 }
+
 
   
 
--- src/homework_1/Homework_1.java
+++ src/homework_1/Homework_1.java
@@ -1,5 +1,29 @@
 package homework_1;
 
 public class Homework_1 {
-
+	public static void main(String[] args) {
+		//1
+    	int age = 18;
+		 if (age >= 18) {
+	         System.out.println("Вы совершеннолетний");
+		 }
+		 //2
+		
+		 int temperature = 0;
+		
+		 if (temperature < 0) {
+            System.out.println("Холодно, одевайтесь теплее");
+		 } else {
+	        System.out.println("Холодно, одевайтесь теплее");
+		 }
+		 //3
+		 int mark = 4
+		 if (mark > 4)
+		 System.out.println("Great !");
+	     } else if (mark < 5) {
+	     System.out.println("Normal!");
+	     }else {
+	     System.out.println("Bad !");
+	    }
+	}
 }
